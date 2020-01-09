bf4e1a4ab0c72b30b3e3d81d9456dd2fd5a51748
Mayssem CHOUAIBI <mch@iga.tn.com>
2020-01-09 15:47:59 +0100

    update project form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import AddProject from './components/Project/AddProject';
 import store from './store'
+import UpdateProject from './components/Project/UpdateProject';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Header />
         <Route exact path="/dashboard" component={Dashboard}/>
         <Route exact path="/addProject" component={AddProject}/>        
+        <Route exact path="/updateProject/:id" component={UpdateProject}/>        
       </Router>
     </Provider>
   );
--- ppmtool-react-client/src/components/Project/ProjectItem.js
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-
 import { Link } from 'react-router-dom'
+
 class ProjectItem extends Component { 
     render() {
         const {project} = this.props;
@@ -22,7 +22,7 @@ class ProjectItem extends Component {
                                         <i className="fa fa-flag-checkered pr-1">Project Board </i>
                                     </li>
                                 </Link>
-                                <Link to="#">
+                                <Link to={`/updateProject/${project.projectIdentifier}`}>
                                     <li className="list-group-item update">
                                         <i className="fa fa-edit pr-1">Update Project Info</i>
                                     </li>
