14d3c5f467831fad73fe140f6ed1200fbc6b3f11
Mayssem CHOUAIBI <mch@iga.tn.com>
2020-01-10 10:40:20 +0100

    backlog + project task relation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
@@ -3,6 +3,8 @@ package io.agileintelligence.ppmtool.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -15,6 +17,8 @@ public class Backlog {
     @JoinColumn(name = "projectId", nullable = false)
     @JsonIgnore
     private Project project;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -46,6 +50,14 @@ public class Backlog {
 
     public void setProject(Project project) {
         this.project = project;
+    }
+
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
     }
 
     public Backlog() {
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -21,6 +23,10 @@ public class ProjectTask {
     private String projectIdentifier;
     private Date createdAt;
     private Date updatedAt;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlogId", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     public ProjectTask() {
     }
@@ -52,6 +58,94 @@ public class ProjectTask {
                 ", createdAt=" + createdAt +
                 ", updatedAt=" + updatedAt +
                 '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectSequence() {
+        return projectSequence;
+    }
+
+    public void setProjectSequence(String projectSequence) {
+        this.projectSequence = projectSequence;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @PrePersist
